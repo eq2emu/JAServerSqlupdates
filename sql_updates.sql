e866ff56cc616adb14911e41e6da2e66da78ce8f
Cynnar <eq2dev@eq2dev.mmoemulators.local>
2020-04-04 13:01:55 -0700

    auto synced on 040420

--- sql_updates.sql
+++ sql_updates.sql
@@ -1,3 +1,6 @@
 delete FROM spawn_location_placement where spawn_location_id=298903;
 delete FROM spawn_location_name where id=298903;
 delete FROM spawn_location_entry where spawn_id=330273 and spawn_location_id = 298903;
+delete FROM spawn_location_placement where spawn_location_id=298824;
+delete FROM spawn_location_name where id=298824;
+delete FROM spawn_location_entry where spawn_id=330273 and spawn_location_id = 298824;
