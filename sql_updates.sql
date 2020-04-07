569011a1bf6820bc71691527f2f55576111f3b3b
Cynnar <eq2dev@eq2dev.mmoemulators.local>
2020-04-07 02:04:01 -0700

    auto synced on 040720

--- sql_updates.sql
+++ sql_updates.sql
@@ -1,1 +1,1 @@
-INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 428999, 'QC024_01_ALargeCoastalCrab428999' FROM spawn_location_group;
+INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 429023, 'QC024_01_ALargeCoastalCrab429023' FROM spawn_location_group;
