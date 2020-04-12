78cf81f1ee417c844fb56f0c9a1f5970fb8f4bfc
Cynnar <jacob0305@gmail.com>
2020-04-12 08:04:01 -0700

    auto synced on 041220

--- sql_updates_041220.sql
+++ sql_updates_041220.sql
@@ -1,2 +1,1 @@
-INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 429012, 'QC025_01_AGiantCoastalCrab429012' FROM spawn_location_group;
-INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 429013, 'QC025_01_AGiantCoastalCrab429013' FROM spawn_location_group;
+INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 429014, 'QC025_01_AGiantCoastalCrab429014' FROM spawn_location_group;
