8f157feccffc7c21e51bfae0f32cefe495166e0d
Cynnar <jacob0305@gmail.com>
2020-04-18 02:04:01 -0700

    auto synced on 041820

---
+++ sql_updates_041820.sql
@@ -0,0 +1,3 @@
+insert into spawn_location_name (id, name) values(1587514, 'aMorakDevourerAttack');
+insert into spawn_location_entry (spawn_id, spawn_location_id, spawnpercentage) values(2530015, 1587514, 0);
+insert into spawn_location_placement (zone_id, spawn_location_id, x, y, z, x_offset, y_offset, z_offset, heading, grid_id) values(253, 1587514, 265.369995, -4.460000, 58.080002, 0.000000, 0.000000, 0.000000, 153.406250, 1246288135);
