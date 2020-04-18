ab4370e40fac88a596338f10d74e9a0082828ec5
Cynnar <jacob0305@gmail.com>
2020-04-18 03:04:01 -0700

    auto synced on 041820

--- sql_updates_041820.sql
+++ sql_updates_041820.sql
@@ -1,3 +1,6 @@
-insert into spawn_location_name (id, name) values(1587514, 'aMorakDevourerAttack');
-insert into spawn_location_entry (spawn_id, spawn_location_id, spawnpercentage) values(2530015, 1587514, 0);
-insert into spawn_location_placement (zone_id, spawn_location_id, x, y, z, x_offset, y_offset, z_offset, heading, grid_id) values(253, 1587514, 265.369995, -4.460000, 58.080002, 0.000000, 0.000000, 0.000000, 153.406250, 1246288135);
+INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 428874, 'MorakDevourerAttack01' FROM spawn_location_group;
+insert ignore into spawn_location_group (group_id, placement_id) values(1044835, 0);
+insert ignore into spawn_location_group (group_id, placement_id) values(1044835, 0);
+INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 433081, 'LieutenantWestfallGroup' FROM spawn_location_group;
+insert ignore into spawn_location_group (group_id, placement_id) values(1044836, 433171);
+insert ignore into spawn_location_group (group_id, placement_id) values(1044836, 433172);
