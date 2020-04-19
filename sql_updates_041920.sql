7cc4e56093ae0a31f2f369d871a31f7291941a97
Cynnar <jacob0305@gmail.com>
2020-04-19 06:04:01 -0700

    auto synced on 041920

---
+++ sql_updates_041920.sql
@@ -0,0 +1,7 @@
+INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 428032, 'QC026_01_AGoveDeer425032' FROM spawn_location_group;
+INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 428085, 'QC026_01_AGoveDeer425085' FROM spawn_location_group;
+insert into spawn (id, name, race, model_type, size, targetable, show_name, command_primary, command_secondary, visual_state, attackable, show_level, show_command_icon, display_hand_icon, faction_id, collision_radius, hp, power, prefix, suffix, last_name) values(2530198, 'a grove deer', 255, 206, 16, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 32, 26, 26, '', '', '');
+insert into spawn_npcs (spawn_id, min_level, max_level, enc_level, class_, gender, min_group_size, max_group_size, hair_type_id, facial_hair_type_id, wing_type_id, chest_type_id, legs_type_id, soga_hair_type_id, soga_facial_hair_type_id, soga_model_type, heroic_flag, action_state, mood_state, initial_state, activity_status, hide_hood, emote_state) values(2530198, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 206, 0, 0, 0, 0, 0, 0, 0);
+insert into spawn_location_name (id, name) values(1587516, 'a');
+insert into spawn_location_entry (spawn_id, spawn_location_id, spawnpercentage) values(2530198, 1587516, 100);
+insert into spawn_location_placement (zone_id, spawn_location_id, x, y, z, x_offset, y_offset, z_offset, heading, grid_id) values(253, 1587516, 72.414268, -3.797499, 217.979324, 0.000000, 0.000000, 0.000000, 257.968750, 771354606);
