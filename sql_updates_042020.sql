71478c459422ae9da2069e5d816e06f78e0ad0f9
Cynnar <jacob0305@gmail.com>
2020-04-20 11:04:01 -0700

    auto synced on 042020

--- sql_updates_042020.sql
+++ sql_updates_042020.sql
@@ -1,4 +1,2 @@
-insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1044849, 1044847);
-insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1044850, 1044847);
-insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1044851, 1044847);
-INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 432799, 'QC027_01_ASeasideFalcon432799' FROM spawn_location_group;
+INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 432822, 'QC027_01_ASeasideFalcon432822' FROM spawn_location_group;
+INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 432842, 'QC027_01_ASeasideFalcon432842' FROM spawn_location_group;
