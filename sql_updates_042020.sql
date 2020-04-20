ae9f6ec8309f694d93187e0ec52ba6f4d5f46786
Cynnar <jacob0305@gmail.com>
2020-04-20 13:04:01 -0700

    auto synced on 042020

--- sql_updates_042020.sql
+++ sql_updates_042020.sql
@@ -1,7 +1,4 @@
-INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 432859, 'QC027_01_ASeasideFalcon432859' FROM spawn_location_group;
-INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 432866, 'QC027_01_ASeasideFalcon432866' FROM spawn_location_group;
-insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1044853, 1044852);
-insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1044854, 1044852);
-insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1044855, 1044852);
-insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1044856, 1044852);
-INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 432894, 'QC027_02_ASeasideFalcon432894' FROM spawn_location_group;
+INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 432911, 'QC027_02_ASeasideFalcon432911' FROM spawn_location_group;
+INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 432922, 'QC027_02_ASeasideFalcon432922' FROM spawn_location_group;
+INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 432925, 'QC027_02_ASeasideFalcon432925' FROM spawn_location_group;
+INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 432928, 'QC027_02_ASeasideFalcon432928' FROM spawn_location_group;
