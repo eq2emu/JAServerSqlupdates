88776cf4f4c9f51915e0a575e5354de6bf7b11c4
Cynnar <jacob0305@gmail.com>
2020-04-22 11:04:01 -0700

    auto synced on 042220

---
+++ sql_updates_042220.sql
@@ -0,0 +1,2 @@
+INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 428038, 'QC028_01_AGroveDeer428038' FROM spawn_location_group;
+INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 428039, 'QC028_01_AGroveDeer428039' FROM spawn_location_group;
