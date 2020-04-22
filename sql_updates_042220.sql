eafafc135a7fa86217d9b3b4acde3d88b6a6e7c7
Cynnar <jacob0305@gmail.com>
2020-04-22 12:04:01 -0700

    auto synced on 042220

--- sql_updates_042220.sql
+++ sql_updates_042220.sql
@@ -1,2 +1,8 @@
-INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 428038, 'QC028_01_AGroveDeer428038' FROM spawn_location_group;
-INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 428039, 'QC028_01_AGroveDeer428039' FROM spawn_location_group;
+INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 428040, 'QC028_01_AGroveDeer428040' FROM spawn_location_group;
+INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 428041, 'QC028_01_AGroveDeer428041' FROM spawn_location_group;
+INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 428042, 'QC028_01_AGroveDeer428042' FROM spawn_location_group;
+insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1044868, 1044867);
+insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1044869, 1044867);
+insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1044870, 1044867);
+insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1044871, 1044867);
+INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 428043, 'QC028_02_AGroveDeer428043' FROM spawn_location_group;
