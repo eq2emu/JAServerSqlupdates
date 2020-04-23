e754f5961576a4996eeea02c002a2c3ff34cc9dc
Cynnar <jacob0305@gmail.com>
2020-04-23 12:04:01 -0700

    auto synced on 042320

--- sql_updates_042320.sql
+++ sql_updates_042320.sql
@@ -1,6 +1,2 @@
-INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 428047, 'QC028_02_AGroveDeer428047' FROM spawn_location_group;
-insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1044873, 1044872);
-insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1044874, 1044872);
-insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1044875, 1044872);
-insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1044876, 1044872);
-INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 432803, 'QC029_01_ASeasideFalcon432803' FROM spawn_location_group;
+update spawn_location_placement set x=-82.529716, y=-17.552717, z=-213.132370, heading=6.000000, x_offset=0.000000, y_offset=0.000000, z_offset=0.000000, respawn=300, expire_timer=0, expire_offset=0, grid_id=16970, pitch=0.000000, roll=0.000000 where id = 404695;
+update spawn_location_placement set x=-82.620636, y=-17.552717, z=-213.151367, heading=354.000000, x_offset=0.000000, y_offset=0.000000, z_offset=0.000000, respawn=300, expire_timer=0, expire_offset=0, grid_id=16970, pitch=0.000000, roll=0.000000 where id = 404695;
