7aebbe0ebd0e10afb6f9e6703b6c8ec13a09bd95
Cynnar <jacob0305@gmail.com>
2020-04-25 09:04:01 -0700

    auto synced on 042520

--- sql_updates_042520.sql
+++ sql_updates_042520.sql
@@ -1,1 +1,3 @@
-INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 432808, 'QC029_02_ASeasideFalcon432808' FROM spawn_location_group;
+INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 432809, 'QC029_02_ASeasideFalcon432809' FROM spawn_location_group;
+INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 428943, 'QC029_02_ATaintedFalcon428943' FROM spawn_location_group;
+INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 432811, 'QC029_02_ASeasideFalcon432811' FROM spawn_location_group;
