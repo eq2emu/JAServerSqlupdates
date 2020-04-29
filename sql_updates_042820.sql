cd569c6875229ede08865aa1f51ba5be2c825e62
Cynnar <jacob0305@gmail.com>
2020-04-28 19:04:01 -0700

    auto synced on 042820

--- sql_updates_042820.sql
+++ sql_updates_042820.sql
@@ -1,6 +1,3 @@
-insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1044939, 1044938);
-insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1044940, 1044938);
-insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1044942, 1044941);
-insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1044943, 1044941);
-insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1044945, 1044944);
-insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1044946, 1044944);
+delete FROM spawn_location_placement where spawn_location_id=409032;
+delete FROM spawn_location_name where id=409032;
+delete FROM spawn_location_entry where spawn_id=1270014 and spawn_location_id = 409032;
