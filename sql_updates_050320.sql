91306c1d23f8b7b81c66f4ff627d2e056c114b2f
Cynnar <jacob0305@gmail.com>
2020-05-03 14:04:01 -0700

    auto synced on 050320

--- sql_updates_050320.sql
+++ sql_updates_050320.sql
@@ -1,3 +1,2 @@
-INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 430898, 'QC035_01_AToxicCrawler430898' FROM spawn_location_group;
-INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 430930, 'QC035_01_AToxicCrawler430930' FROM spawn_location_group;
-INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 431021, 'QC035_01_AToxicCrawler431021' FROM spawn_location_group;
+insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1044973, 1044972);
+insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1044974, 1044972);
