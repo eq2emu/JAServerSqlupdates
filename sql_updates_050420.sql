94342eea61ad59a9c82ad8a126d3de682382b623
Cynnar <jacob0305@gmail.com>
2020-05-04 14:04:01 -0700

    auto synced on 050420

--- sql_updates_050420.sql
+++ sql_updates_050420.sql
@@ -1,5 +1,11 @@
-INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 430875, 'QC035_02_AToxicCrawler430875' FROM spawn_location_group;
-INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 430879, 'QC035_02_AToxicCrawler430879' FROM spawn_location_group;
-insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1044976, 1044975);
-insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1044977, 1044975);
-INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 430880, 'QC035_03_AToxicCrawler430880' FROM spawn_location_group;
+INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 430883, 'QC035_03_AToxicCrawler430883' FROM spawn_location_group;
+INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 430884, 'QC035_03_AToxicCrawler430884' FROM spawn_location_group;
+insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1044979, 1044978);
+insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1044980, 1044978);
+INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 430885, 'QC035_04_AToxicCrawler430885' FROM spawn_location_group;
+INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 430886, 'QC035_04_AToxicCrawler430886' FROM spawn_location_group;
+INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 430887, 'QC035_04_AToxicCrawler430887' FROM spawn_location_group;
+insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1044982, 1044981);
+insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1044983, 1044981);
+INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 430891, 'QC035_05_AToxicCrawler430891' FROM spawn_location_group;
+INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 430892, 'QC035_05_AToxicCrawler430892' FROM spawn_location_group;
