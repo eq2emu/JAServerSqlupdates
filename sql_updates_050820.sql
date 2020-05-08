d36b792e68cec5b27c1efd633c725bc1302c4450
Cynnar <jacob0305@gmail.com>
2020-05-08 00:04:01 -0700

    auto synced on 050820

---
+++ sql_updates_050820.sql
@@ -0,0 +1,5 @@
+insert into spawn (id, name, race, model_type, size, targetable, show_name, command_primary, command_secondary, visual_state, attackable, show_level, show_command_icon, display_hand_icon, faction_id, collision_radius, hp, power, prefix, suffix, last_name) values(1950128, 'menders anvil', 255, 41, 32, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 26, 26, '', '', '');
+insert into spawn_objects (spawn_id) values(1950128);
+insert into spawn_location_name (id, name) values(1587589, 'mendersanvil');
+insert into spawn_location_entry (spawn_id, spawn_location_id, spawnpercentage) values(1950128, 1587589, 100);
+insert into spawn_location_placement (zone_id, spawn_location_id, x, y, z, x_offset, y_offset, z_offset, heading, grid_id) values(195, 1587589, 990.603760, 2.161243, -348.388519, 0.000000, 0.000000, 0.000000, 327.593750, 32099134);
