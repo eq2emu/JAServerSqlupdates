64d0a6022c770090094440cbf3af66a6970cae37
Cynnar <jacob0305@gmail.com>
2020-05-08 11:04:01 -0700

    auto synced on 050820

--- sql_updates_050820.sql
+++ sql_updates_050820.sql
@@ -1,6 +1,9 @@
-INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 428365, 'QC038_03_AGhostlySapling428365' FROM spawn_location_group;
-INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 428366, 'QC038_03_AGhostlySapling428366' FROM spawn_location_group;
-insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1045011, 1045010);
-insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1045012, 1045010);
-INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 428367, 'QC038_04_AGhostlySapling428367' FROM spawn_location_group;
-INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 428368, 'QC038_04_AGhostlySapling428368' FROM spawn_location_group;
+INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 428369, 'QC038_04_AGhostlySapling428369' FROM spawn_location_group;
+insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1045014, 1045013);
+insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1045015, 1045013);
+INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 428287, 'QC039_01_AGhostlyTreant428287' FROM spawn_location_group;
+INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 428291, 'QC039_01_AGhostlyTreant428291' FROM spawn_location_group;
+INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 428300, 'QC039_01_AGhostlyTreant428300' FROM spawn_location_group;
+insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1045017, 1045016);
+insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1045018, 1045016);
+INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 428325, 'QC039_02_AGhostlyTreant428325' FROM spawn_location_group;
