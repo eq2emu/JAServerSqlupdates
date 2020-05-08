cbdcef4650e861a0df1ae49bed08c88e8b177483
Cynnar <jacob0305@gmail.com>
2020-05-08 04:04:01 -0700

    auto synced on 050820

--- sql_updates_050820.sql
+++ sql_updates_050820.sql
@@ -1,6 +1,5 @@
-insert into spawn_location_name (id, name) values(1587591, 'sack of coal');
-insert into spawn_location_entry (spawn_id, spawn_location_id, spawnpercentage) values(4701805, 1587591, 100);
-insert into spawn_location_placement (zone_id, spawn_location_id, x, y, z, x_offset, y_offset, z_offset, heading, grid_id) values(195, 1587591, 987.963135, 2.116690, -348.018860, 0.000000, 0.000000, 0.000000, 276.046875, 32099134);
-insert into spawn_location_name (id, name) values(1587592, 'lucky wolfpaw recipe');
-insert into spawn_location_entry (spawn_id, spawn_location_id, spawnpercentage) values(4701804, 1587592, 100);
-insert into spawn_location_placement (zone_id, spawn_location_id, x, y, z, x_offset, y_offset, z_offset, heading, grid_id) values(195, 1587592, 988.704712, 2.125386, -348.850586, 0.000000, 0.000000, 0.000000, 305.031250, 32099134);
+insert into spawn (id, name, race, model_type, size, targetable, show_name, command_primary, command_secondary, visual_state, attackable, show_level, show_command_icon, display_hand_icon, faction_id, collision_radius, hp, power, prefix, suffix, last_name) values(1950130, 'Calla Badgerbane', 255, 12, 32, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 32, 26, 26, '', '', '');
+insert into spawn_npcs (spawn_id, min_level, max_level, enc_level, class_, gender, min_group_size, max_group_size, hair_type_id, facial_hair_type_id, wing_type_id, chest_type_id, legs_type_id, soga_hair_type_id, soga_facial_hair_type_id, soga_model_type, heroic_flag, action_state, mood_state, initial_state, activity_status, hide_hood, emote_state) values(1950130, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 12, 0, 0, 0, 0, 0, 0, 0);
+insert into spawn_location_name (id, name) values(1587593, 'Calla Badgerbane');
+insert into spawn_location_entry (spawn_id, spawn_location_id, spawnpercentage) values(1950130, 1587593, 100);
+insert into spawn_location_placement (zone_id, spawn_location_id, x, y, z, x_offset, y_offset, z_offset, heading, grid_id) values(195, 1587593, 985.437073, 2.160944, -344.804382, 0.000000, 0.000000, 0.000000, 143.890625, 32099134);
