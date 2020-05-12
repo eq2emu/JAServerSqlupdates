3048ef3ccfe7edbf2821e92754b7699eb6f18fa3
Cynnar <jacob0305@gmail.com>
2020-05-12 08:04:01 -0700

    auto synced on 051220

---
+++ sql_updates_051220.sql
@@ -0,0 +1,5 @@
+insert into spawn (id, name, race, model_type, size, targetable, show_name, command_primary, command_secondary, visual_state, attackable, show_level, show_command_icon, display_hand_icon, faction_id, collision_radius, hp, power, prefix, suffix, last_name) values(2530204, 'a dead deer', 255, 206, 32, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 76, 76, '', '', '');
+insert into spawn_objects (spawn_id) values(2530204);
+insert into spawn_location_name (id, name) values(1587604, 'QueensColony_ADeadDeerObject');
+insert into spawn_location_entry (spawn_id, spawn_location_id, spawnpercentage) values(2530204, 1587604, 100);
+insert into spawn_location_placement (zone_id, spawn_location_id, x, y, z, x_offset, y_offset, z_offset, heading, grid_id) values(253, 1587604, 185.145752, 1.105838, 60.491261, 0.000000, 0.000000, 0.000000, 34.890625, 3146737430);
