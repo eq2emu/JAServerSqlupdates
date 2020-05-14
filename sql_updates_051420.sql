830499d64faf13d9275e19879b61cca0b697a294
Cynnar <jacob0305@gmail.com>
2020-05-14 01:04:01 -0700

    auto synced on 051420

---
+++ sql_updates_051420.sql
@@ -0,0 +1,9 @@
+insert into spawn_location_name (id, name) values(1587606, 'new');
+insert into spawn_location_entry (spawn_id, spawn_location_id, spawnpercentage) values(1960026, 1587606, 100);
+insert into spawn_location_placement (zone_id, spawn_location_id, x, y, z, x_offset, y_offset, z_offset, heading, grid_id) values(196, 1587606, 980.707336, -19.256397, -709.041748, 0.000000, 0.000000, 0.000000, 132.062500, 1244619831);
+insert into spawn_location_name (id, name) values(1587607, 'new');
+insert into spawn_location_entry (spawn_id, spawn_location_id, spawnpercentage) values(1960026, 1587607, 100);
+insert into spawn_location_placement (zone_id, spawn_location_id, x, y, z, x_offset, y_offset, z_offset, heading, grid_id) values(196, 1587607, 961.752136, -21.963247, -710.971558, 0.000000, 0.000000, 0.000000, 98.515625, 1244619831);
+insert into spawn_location_name (id, name) values(1587608, 'new');
+insert into spawn_location_entry (spawn_id, spawn_location_id, spawnpercentage) values(1960026, 1587608, 100);
+insert into spawn_location_placement (zone_id, spawn_location_id, x, y, z, x_offset, y_offset, z_offset, heading, grid_id) values(196, 1587608, 968.015747, -20.580940, -701.030090, 0.000000, 0.000000, 0.000000, 180.765625, 1244619831);
