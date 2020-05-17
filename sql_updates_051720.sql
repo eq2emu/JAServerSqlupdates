d5e106864cb0862bfb8954981aba07a2e380f1a9
Cynnar <jacob0305@gmail.com>
2020-05-17 09:04:01 -0700

    auto synced on 051720

--- sql_updates_051720.sql
+++ sql_updates_051720.sql
@@ -1,7 +1,1 @@
-update spawn_npcs, spawn set name='a Gigglegibber Goblin Gamblin\' Game Vendor', min_level=65, max_level=65, enc_level=3, race=255, model_type=7039, class_=7, gender=1, show_name=1, attackable=0, show_level=0, targetable=1, show_command_icon=1, display_hand_icon=0, hair_type_id=0, facial_hair_type_id=0, wing_type_id=0, chest_type_id=0, legs_type_id=0, soga_hair_type_id=0, soga_facial_hair_type_id=0, soga_model_type=7039, size=20, hp=976, heroic_flag=0, power=976, collision_radius=35, command_primary=43, command_secondary=0, visual_state=0, action_state=0, mood_state=0, initial_state=16512, activity_status=0, alignment=0, faction_id=0, hide_hood=0, emote_state=0, suffix ='', prefix='', last_name='' where spawn_npcs.spawn_id = spawn.id and spawn.id = 120001;
-delete FROM spawn_location_placement where spawn_location_id=194480;
-delete FROM spawn_location_name where id=194480;
-delete FROM spawn_location_entry where spawn_id=120456 and spawn_location_id = 194480;
-delete FROM spawn_location_placement where spawn_location_id=593322;
-delete FROM spawn_location_name where id=593322;
-delete FROM spawn_location_entry where spawn_id=121502 and spawn_location_id = 593322;
+update spawn_objects, spawn set name='qst_news_for_germain_leaf_sample', model_type=3908, show_name=1, targetable=1, size=38, command_primary=0, command_secondary=0, visual_state=0, attackable=0, show_level=0, show_command_icon=1, display_hand_icon=1, collision_radius=37, hp = 0, power = 0, device_id = 0 where spawn_objects.spawn_id = spawn.id and spawn.id = 1960154;
