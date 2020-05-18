d15d696f82f63fbcfa715753b18f6ea17f65e700
Cynnar <jacob0305@gmail.com>
2020-05-18 12:04:01 -0700

    auto synced on 051820

--- sql_updates_051820.sql
+++ sql_updates_051820.sql
@@ -1,7 +1,3 @@
-update spawn_location_placement set x=187.670593, y=1.305377, z=26.208487, heading=232.000000, x_offset=0.000000, y_offset=0.000000, z_offset=0.000000, respawn=45, expire_timer=0, expire_offset=0, grid_id=3146737430, pitch=0.000000, roll=0.000000 where id = 429344;
-update spawn_location_placement set x=187.521423, y=1.305710, z=26.247860, heading=243.984375, x_offset=0.000000, y_offset=0.000000, z_offset=0.000000, respawn=45, expire_timer=0, expire_offset=0, grid_id=3146737430, pitch=0.000000, roll=0.000000 where id = 429344;
-update spawn_location_placement set x=188.918091, y=1.301165, z=25.466909, heading=176.000000, x_offset=0.000000, y_offset=0.000000, z_offset=0.000000, respawn=45, expire_timer=0, expire_offset=0, grid_id=3146737430, pitch=0.000000, roll=0.000000 where id = 429368;
-update spawn_location_placement set x=200.173584, y=1.254145, z=50.445198, heading=67.687500, x_offset=0.000000, y_offset=0.000000, z_offset=0.000000, respawn=45, expire_timer=0, expire_offset=0, grid_id=3146737430, pitch=0.000000, roll=0.000000 where id = 1584930;
-update spawn_location_placement set x=198.152802, y=1.261561, z=50.636028, heading=335.968750, x_offset=0.000000, y_offset=0.000000, z_offset=0.000000, respawn=45, expire_timer=0, expire_offset=0, grid_id=3146737430, pitch=0.000000, roll=0.000000 where id = 429371;
-update spawn_location_placement set x=198.015549, y=1.262302, z=50.708206, heading=335.968750, x_offset=0.000000, y_offset=0.000000, z_offset=0.000000, respawn=45, expire_timer=0, expire_offset=0, grid_id=3146737430, pitch=0.000000, roll=0.000000 where id = 429371;
-update spawn_location_placement set x=176.611679, y=1.335330, z=76.998466, heading=343.828125, x_offset=0.000000, y_offset=0.000000, z_offset=0.000000, respawn=45, expire_timer=0, expire_offset=0, grid_id=3146737430, pitch=0.000000, roll=0.000000 where id = 1584905;
+delete FROM spawn_location_placement where spawn_location_id=401793;
+delete FROM spawn_location_name where id=401793;
+delete FROM spawn_location_entry where spawn_id=2780097 and spawn_location_id = 401793;
