ae23d68139471c9d099a14e149a3b019de0e0ac9
Cynnar <jacob0305@gmail.com>
2020-05-18 13:04:01 -0700

    auto synced on 051820

--- sql_updates_051820.sql
+++ sql_updates_051820.sql
@@ -1,3 +1,3 @@
-delete FROM spawn_location_placement where spawn_location_id=401793;
-delete FROM spawn_location_name where id=401793;
-delete FROM spawn_location_entry where spawn_id=2780097 and spawn_location_id = 401793;
+insert into spawn_location_name (id, name) values(1587613, 'Laena');
+insert into spawn_location_entry (spawn_id, spawn_location_id, spawnpercentage) values(2780097, 1587613, 100);
+insert into spawn_location_placement (zone_id, spawn_location_id, x, y, z, x_offset, y_offset, z_offset, heading, grid_id) values(278, 1587613, 250.520004, -3.960000, -4.730000, 0.000000, 0.000000, 0.000000, 263.828125, 1246288135);
