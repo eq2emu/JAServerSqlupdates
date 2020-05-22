7adb7e5d10c3fb3163bf6d1752d4de1ce26c7020
Cynnar <jacob0305@gmail.com>
2020-05-22 01:04:01 -0700

    auto synced on 052220

---
+++ sql_updates_052220.sql
@@ -0,0 +1,3 @@
+insert into spawn_location_name (id, name) values(1587626, 'QueensColony_ATaintedSapswillTapper');
+insert into spawn_location_entry (spawn_id, spawn_location_id, spawnpercentage) values(2530021, 1587626, 100);
+insert into spawn_location_placement (zone_id, spawn_location_id, x, y, z, x_offset, y_offset, z_offset, heading, grid_id) values(253, 1587626, 154.511429, 0.940634, 16.672155, 0.000000, 0.000000, 0.000000, 70.812500, 3146737430);
