d718170609162eefacf13a378e232c69628b70db
Cynnar <jacob0305@gmail.com>
2020-05-24 17:04:01 -0700

    auto synced on 052420

--- sql_updates_052420.sql
+++ sql_updates_052420.sql
@@ -1,1 +1,6 @@
-update spawn_location_placement set x=-8.803481, y=0.002500, z=-6.926004, heading=230.000000, x_offset=0.000000, y_offset=0.000000, z_offset=0.000000, respawn=300, expire_timer=0, expire_offset=0, grid_id=3240910078, pitch=0.000000, roll=0.000000 where id = 433250;
+delete FROM spawn_location_placement where spawn_location_id=433250;
+delete FROM spawn_location_name where id=433250;
+delete FROM spawn_location_entry where spawn_id=3480005 and spawn_location_id = 433250;
+insert into spawn_location_name (id, name) values(1587665, 'Tayil');
+insert into spawn_location_entry (spawn_id, spawn_location_id, spawnpercentage) values(3480005, 1587665, 100);
+insert into spawn_location_placement (zone_id, spawn_location_id, x, y, z, x_offset, y_offset, z_offset, heading, grid_id) values(348, 1587665, -9.400000, 0.000000, -6.480000, 0.000000, 0.000000, 0.000000, 258.906250, 3240910078);
