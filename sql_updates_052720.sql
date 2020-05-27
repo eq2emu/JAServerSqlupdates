7484f56e064d6ce542e6449ecf702896f06ddf53
Cynnar <jacob0305@gmail.com>
2020-05-27 14:04:01 -0700

    auto synced on 052720

--- sql_updates_052720.sql
+++ sql_updates_052720.sql
@@ -1,2 +1,6 @@
-update spawn_location_placement set x=51.504097, y=5.011900, z=-33.431248, heading=336.000000, x_offset=0.000000, y_offset=0.000000, z_offset=0.000000, respawn=300, expire_timer=0, expire_offset=0, grid_id=658250038, pitch=0.000000, roll=0.000000 where id = 308908;
-update spawn_location_placement set x=33.865593, y=5.252505, z=-16.021008, heading=60.015625, x_offset=0.000000, y_offset=0.000000, z_offset=0.000000, respawn=300, expire_timer=0, expire_offset=0, grid_id=658250038, pitch=0.000000, roll=0.000000 where id = 308908;
+update spawn_objects, spawn set name='barricade_short', model_type=4479, show_name=0, targetable=1, size=32, command_primary=0, command_secondary=0, visual_state=0, attackable=0, show_level=0, show_command_icon=0, display_hand_icon=0, collision_radius=32, hp = 0, power = 0, device_id = 0 where spawn_objects.spawn_id = spawn.id and spawn.id = 6470025;
+insert into spawn (id, name, race, model_type, size, targetable, show_name, command_primary, command_secondary, visual_state, attackable, show_level, show_command_icon, display_hand_icon, faction_id, collision_radius, hp, power, prefix, suffix, last_name) values(6470028, '0', 255, 1949, 5, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, '', '', '');
+insert into spawn_objects (spawn_id) values(6470028);
+insert into spawn_location_name (id, name) values(1587666, 'stolen');
+insert into spawn_location_entry (spawn_id, spawn_location_id, spawnpercentage) values(6470028, 1587666, 100);
+insert into spawn_location_placement (zone_id, spawn_location_id, x, y, z, x_offset, y_offset, z_offset, heading, grid_id) values(647, 1587666, 32.297100, 2.002546, 15.384293, 0.000000, 0.000000, 0.000000, 101.843750, 1275535964);
