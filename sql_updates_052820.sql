1db843cb91db4e482b89da0f1216191380c181fc
Cynnar <jacob0305@gmail.com>
2020-05-28 17:04:01 -0700

    auto synced on 052820

---
+++ sql_updates_052820.sql
@@ -0,0 +1,1 @@
+update spawn_objects, spawn set name='my new name', model_type=3908, show_name=1, targetable=1, size=38, command_primary=1140, command_secondary=0, visual_state=0, attackable=0, show_level=0, show_command_icon=0, display_hand_icon=0, collision_radius=37, hp = 0, power = 0, device_id = 0 where spawn_objects.spawn_id = spawn.id and spawn.id = 1960182;
