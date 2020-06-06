895c6b79de4493492fcb66e745febe98034777ad
Cynnar <jacob0305@gmail.com>
2020-06-06 05:04:01 -0700

    auto synced on 060620

--- sql_updates_060620.sql
+++ sql_updates_060620.sql
@@ -1,7 +1,1 @@
-INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 428699, 'QC044_01_AMorakDevourer428699' FROM spawn_location_group;
-insert ignore into spawn_location_group (group_id, placement_id) values(1045104, 428718);
-insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1045100, 1045099);
-insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1045101, 1045099);
-insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1045102, 1045099);
-insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1045103, 1045099);
-insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1045104, 1045099);
+INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 428683, 'QC045_01_AMorakDevourer428663' FROM spawn_location_group;
