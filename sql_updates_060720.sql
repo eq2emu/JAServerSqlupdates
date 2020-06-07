69112b4a6db650a23670e7f1939e19b25c707c78
Cynnar <jacob0305@gmail.com>
2020-06-07 11:04:01 -0700

    auto synced on 060720

--- sql_updates_060720.sql
+++ sql_updates_060720.sql
@@ -1,7 +1,4 @@
-insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1045107, 1045106);
-insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1045108, 1045106);
-insert ignore into spawn_location_group_associations (group_id1, group_id2) values(1045109, 1045106);
-insert into spawn_location_name (id, name) values(1587698, 'QueensColony_ATaintedFalcon');
-insert into spawn_location_entry (spawn_id, spawn_location_id, spawnpercentage) values(2530016, 1587698, 100);
-insert into spawn_location_placement (zone_id, spawn_location_id, x, y, z, x_offset, y_offset, z_offset, heading, grid_id) values(253, 1587698, 237.778534, -1.051749, -65.637505, 0.000000, 0.000000, 0.000000, 162.671875, 1246288135);
-INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 1587774, 'QC046_02_ATaintedFalcon1587698' FROM spawn_location_group;
+insert into spawn_location_name (id, name) values(1587699, 'QueensColony_ATaintedFalcon');
+insert into spawn_location_entry (spawn_id, spawn_location_id, spawnpercentage) values(2530016, 1587699, 100);
+insert into spawn_location_placement (zone_id, spawn_location_id, x, y, z, x_offset, y_offset, z_offset, heading, grid_id) values(253, 1587699, 229.300415, -0.827207, -71.789940, 0.000000, 0.000000, 0.000000, 192.250000, 1246288135);
+INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 1587775, 'QC046_02_ATaintedFalcon1587699' FROM spawn_location_group;
