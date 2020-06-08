bef97a7a9f93b3050ebdbdb58b85a73d1ba19931
Cynnar <jacob0305@gmail.com>
2020-06-07 22:04:01 -0700

    auto synced on 060720

--- sql_updates_060720.sql
+++ sql_updates_060720.sql
@@ -1,1 +1,5 @@
-update spawn_location_placement set x=-201.328995, y=-4.116370, z=-359.389008, heading=259.968750, x_offset=0.000000, y_offset=0.000000, z_offset=0.000000, respawn=300, expire_timer=0, expire_offset=0, grid_id=803893359, pitch=0.000000, roll=0.000000 where id = 84448;
+update spawn_objects, spawn set name='lp_roadsign_post_double01', model_type=6603, show_name=0, targetable=1, size=32, command_primary=0, command_secondary=0, visual_state=0, attackable=1, show_level=0, show_command_icon=0, display_hand_icon=0, collision_radius=55, hp = 0, power = 0, device_id = 0 where spawn_objects.spawn_id = spawn.id and spawn.id = 1160187;
+update spawn_objects, spawn set name='lp_roadsign_post_double01', model_type=6603, show_name=0, targetable=0, size=32, command_primary=0, command_secondary=0, visual_state=0, attackable=1, show_level=0, show_command_icon=0, display_hand_icon=0, collision_radius=55, hp = 0, power = 0, device_id = 0 where spawn_objects.spawn_id = spawn.id and spawn.id = 1160187;
+delete FROM spawn_location_placement where spawn_location_id=80524;
+delete FROM spawn_location_name where id=80524;
+delete FROM spawn_location_entry where spawn_id=1080674 and spawn_location_id = 80524;
