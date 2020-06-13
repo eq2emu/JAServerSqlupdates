d27edd34afb348669ae3ebdf7e83b2e3ac99e66f
Cynnar <jacob0305@gmail.com>
2020-06-13 14:04:01 -0700

    auto synced on 061320

--- sql_updates_061320.sql
+++ sql_updates_061320.sql
@@ -1,8 +1,2 @@
-insert into spawn_location_name (id, name) values(133769466, 'QueensColony_ATaintedSapswillFighter');
-insert into spawn_location_entry (spawn_id, spawn_location_id, spawnpercentage) values(2530019, 133769466, 100);
-insert into spawn_location_placement (zone_id, spawn_location_id, x, y, z, x_offset, y_offset, z_offset, heading, grid_id) values(253, 133769466, 241.118271, -3.883449, -12.189431, 0.000000, 0.000000, 0.000000, 62.468750, 1246288135);
-insert into spawn_location_name (id, name) values(133769467, 'QueensColony_ADefiledPriest');
-insert into spawn_location_entry (spawn_id, spawn_location_id, spawnpercentage) values(2530031, 133769467, 100);
-insert into spawn_location_placement (zone_id, spawn_location_id, x, y, z, x_offset, y_offset, z_offset, heading, grid_id) values(253, 133769467, 241.138855, -3.883965, -10.118751, 0.000000, 0.000000, 0.000000, 60.406250, 1246288135);
-INSERT INTO spawn_location_group (group_id, placement_id, name) SELECT IF(ISNULL(MAX(group_id))=1, 100000, MAX(group_id)+1), 1587858, 'QC050_02_ATaintedSapswillFighter133769465' FROM spawn_location_group;
-insert ignore into spawn_location_group (group_id, placement_id) values(1045153, 1587859);
+update spawn_location_placement set x=-246.285004, y=89.588898, z=1404.890015, heading=96.984375, x_offset=0.000000, y_offset=0.000000, z_offset=0.000000, respawn=300, expire_timer=0, expire_offset=0, grid_id=2887769944, pitch=0.000000, roll=0.000000 where id = 199858;
+update spawn_location_placement set x=-242.908997, y=88.515099, z=1410.770020, heading=87.937500, x_offset=0.000000, y_offset=0.000000, z_offset=0.000000, respawn=300, expire_timer=0, expire_offset=0, grid_id=2887769944, pitch=0.000000, roll=0.000000 where id = 200172;
